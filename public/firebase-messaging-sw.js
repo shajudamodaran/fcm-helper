5f57cac4d5900c0c369a2ed6288a52f0170e7e73
Shaju P D <shajupainkulam@gmail.com>
2026-03-11 10:00:47 +0530

    Changes

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,7 +2,14 @@
 importScripts('https://www.gstatic.com/firebasejs/10.14.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.14.0/firebase-messaging-compat.js');
 
-firebase.initializeApp({"apiKey":"AIzaSyBmCrcn5EfMVFO4Kf7vB7fkdZTOO5aImyQ","authDomain":"athercreations.firebaseapp.com","projectId":"athercreations","storageBucket":"athercreations.appspot.com","messagingSenderId":"49258493061","appId":"1:49258493061:web:a741586d7d868d1ce3e5ec"});
+firebase.initializeApp({
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID,
+});
 
 const messaging = firebase.messaging();
 
